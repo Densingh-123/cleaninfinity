ba6751ad228d13de44a8e09af7b7c04597710fe0
shanker2004 <shdeveloper2004@gmailcom>
2024-10-23 00:57:02 +0530

    Added toast and data to options

--- client/src/components/AuthComp.jsx
+++ client/src/components/AuthComp.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { toast } from 'react-toastify'
 
-export default function AuthComponent({ states, districts, wards }) {
+export default function AuthComponent({ states, wards }) {
   const [isSignUp, setIsSignUp] = useState(false)
   const [formData, setFormData] = useState({
     email: '',
@@ -16,23 +16,37 @@ export default function AuthComponent({ states, districts, wards }) {
     otp: '',
   })
   const [otpSent, setOtpSent] = useState(false)
-  const [otpVerified, setOtpVerified] = useState(false) // New state for OTP verification
+  const [otpVerified, setOtpVerified] = useState(false)
+  const [districtsArr, setDistricts] = useState([states.states[0].districts])
 
   const handleChange = (e) => {
     const { name, value } = e.target
     setFormData({ ...formData, [name]: value })
+
+    if (name === 'state') {
+      const selectedState = states.states.find(
+        (stateObj) => stateObj.state === value
+      )
+      if (selectedState) setDistricts(selectedState.districts)
+      else setDistricts([])
+    }
   }
 
   const sendOtp = () => {
-    // Simulate sending OTP
     setOtpSent(true)
     toast.success('OTP sent successfully!')
   }
 
   const verifyOtp = () => {
-    // Simulate OTP verification logic
     setOtpVerified(true)
     toast.success('OTP verified successfully!')
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (otpVerified || isSignUp) {
+      window.location.href = '/dashboard'
+    }
   }
 
   return (
@@ -44,72 +58,25 @@ export default function AuthComponent({ states, districts, wards }) {
         <img src='https://placehold.co/600x400' />
       </div>
       <div className='w-1/3 bg-light-green p-6 rounded-lg shadow-2xl'>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault()
-            console.log(formData)
-          }}
-        >
+        <form onSubmit={handleSubmit}>
           <div className='w-full group authBtn border-2 border-medium-green rounded-lg font-bold mb-4'>
             <button
               type='button'
               className={`animate ${!isSignUp ? 'activeBtn' : 'notActiveBtn'}`}
               onClick={() => setIsSignUp(!isSignUp)}
             >
-              Sign In
+              Sign Up
             </button>
             <button
               type='button'
               className={`animate ${isSignUp ? 'activeBtn' : 'notActiveBtn'}`}
               onClick={() => setIsSignUp(!isSignUp)}
             >
-              Sign Up
+              Sign In
             </button>
           </div>
+
           {!isSignUp ? (
-            <>
-              <input
-                type='text'
-                name='mobile'
-                placeholder='Mobile No'
-                value={formData.mobile}
-                onChange={handleChange}
-                className='w-full mb-2'
-              />
-              <input
-                type='email'
-                name='email'
-                placeholder='Mail Id'
-                value={formData.email}
-                onChange={handleChange}
-                className='w-full mb-2'
-              />
-              <div className='flex items-center mb-2'>
-                <input
-                  type='text'
-                  value='ABC123'
-                  readOnly
-                  className='w-1/2'
-                />
-                <input
-                  type='text'
-                  name='captchaInput'
-                  placeholder='Enter Captcha'
-                  value={formData.captchaInput}
-                  onChange={handleChange}
-                  className='w-1/3 ml-2'
-                />
-              </div>
-              <input
-                type='password'
-                name='password'
-                placeholder='Password'
-                value={formData.password}
-                onChange={handleChange}
-                className='w-full mb-2'
-              />
-            </>
-          ) : (
             <>
               <input
                 type='text'
@@ -117,25 +84,26 @@ export default function AuthComponent({ states, districts, wards }) {
                 placeholder='Name'
                 value={formData.name}
                 onChange={handleChange}
-                className='w-full mb-2'
+                className='w-full'
               />
-              <div className='flex items-center mb-2'>
+              <div className='flex items-center'>
                 <input
                   type='text'
                   name='mobile'
                   placeholder='Mobile Number'
                   value={formData.mobile}
                   onChange={handleChange}
-                  className='w-full mb-0'
+                  className='w-full'
                 />
                 <button
                   type='button'
                   onClick={sendOtp}
-                  className='w-full ml-2 bg-medium-green rounded-lg p-2 text-white font-bold'
+                  className='w-full ml-2 bg-medium-green rounded-lg p-2 text-white font-bold mb-4'
                 >
                   Send OTP
                 </button>
               </div>
+
               {otpSent && !otpVerified && (
                 <>
                   <input
@@ -144,7 +112,7 @@ export default function AuthComponent({ states, districts, wards }) {
                     placeholder='Enter OTP'
                     value={formData.otp}
                     onChange={handleChange}
-                    className='w-full mb-2'
+                    className='w-full'
                   />
                   <button
                     type='button'
@@ -155,7 +123,8 @@ export default function AuthComponent({ states, districts, wards }) {
                   </button>
                 </>
               )}
-              {otpVerified && ( // Show these fields only after OTP is verified
+
+              {otpVerified && (
                 <>
                   <input
                     type='email'
@@ -163,7 +132,7 @@ export default function AuthComponent({ states, districts, wards }) {
                     placeholder='Email'
                     value={formData.email}
                     onChange={handleChange}
-                    className='w-full mb-2'
+                    className='w-full'
                   />
                   <input
                     type='text'
@@ -171,21 +140,21 @@ export default function AuthComponent({ states, districts, wards }) {
                     placeholder='Address'
                     value={formData.address}
                     onChange={handleChange}
-                    className='w-full mb-2'
+                    className='w-full'
                   />
-                  <div className='flex mb-2'>
+                  <div className='flex'>
                     <select
                       name='state'
                       value={formData.state}
                       onChange={handleChange}
                       className='w-1/2 mr-2'
                     >
-                      {states.map((state, index) => (
+                      {states.states.map((stateObj, index) => (
                         <option
                           key={index}
-                          value={state}
+                          value={stateObj.state}
                         >
-                          {state}
+                          {stateObj.state}
                         </option>
                       ))}
                     </select>
@@ -195,7 +164,7 @@ export default function AuthComponent({ states, districts, wards }) {
                       onChange={handleChange}
                       className='w-1/2'
                     >
-                      {districts.map((district, index) => (
+                      {districtsArr.map((district, index) => (
                         <option
                           key={index}
                           value={district}
@@ -209,7 +178,7 @@ export default function AuthComponent({ states, districts, wards }) {
                     name='ward'
                     value={formData.ward}
                     onChange={handleChange}
-                    className='w-full mb-2'
+                    className='w-full'
                   >
                     {wards.map((ward, index) => (
                       <option
@@ -223,17 +192,60 @@ export default function AuthComponent({ states, districts, wards }) {
                 </>
               )}
             </>
+          ) : (
+            <>
+              <input
+                type='text'
+                name='mobile'
+                placeholder='Mobile No'
+                value={formData.mobile}
+                onChange={handleChange}
+                className='w-full'
+              />
+              <input
+                type='email'
+                name='email'
+                placeholder='Mail Id'
+                value={formData.email}
+                onChange={handleChange}
+                className='w-full'
+              />
+              <div className='flex items-center'>
+                <input
+                  type='text'
+                  value='ABC123'
+                  readOnly
+                  className='w-1/2'
+                />
+                <input
+                  type='text'
+                  name='captchaInput'
+                  placeholder='Enter Captcha'
+                  value={formData.captchaInput}
+                  onChange={handleChange}
+                  className='ml-2'
+                />
+              </div>
+              <input
+                type='password'
+                name='password'
+                placeholder='Password'
+                value={formData.password}
+                onChange={handleChange}
+                className='w-full'
+              />
+            </>
           )}
           <button
             type='submit'
-            disabled={otpVerified && isSignUp}
+            disabled={!otpVerified && !isSignUp}
             className={`w-full ${
-              otpVerified
-                ? 'bg-medium-green hover:bg-transparent hover:border-2 hover:text-dark-green hover:shadow-sm'
-                : 'bg-gray-300'
-            } rounded-lg p-2 shadow-lg border-medium-green font-bold mt-4`}
+              otpVerified || isSignUp
+                ? 'bg-medium-green hover:bg-transparent hover:border-2 hover:text-dark-green hover:shadow-sm shadow-lg'
+                : 'bg-dark-green'
+            } rounded-lg p-2 border-medium-green font-bold mt-4`}
           >
-            Get Started
+            {!isSignUp ? 'Get Started' : 'Log in'}
           </button>
         </form>
       </div>
