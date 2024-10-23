4c58b347530f24a88ed4716631b516ed21a0ca83
Densingh <ddensingh19@gmail.com>
2024-10-23 15:55:53 +0530

    Dashboard

--- client/src/components/VerticalCard.jsx
+++ client/src/components/VerticalCard.jsx
@@ -1,18 +1,19 @@
 export default function VerticalCard({ titles }) {
   return (
-    <div className='w-[70%] grid grid-cols-4 items-center justify-center'>
+    <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 items-center justify-center w-full max-w-4xl'>
       {titles.map((title, i) => (
         <div
-          className='p-4 w-11/12 mb-4 items-center justify-center border rounded shadow-lg hover:shadow-md'
+          className='p-4 flex flex-col items-center justify-center bg-white border rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300'
           key={i}
         >
           <img
             src='https://via.placeholder.com/150'
-            className='rounded-lg vert-img'
+            className='w-full h-32 object-cover rounded-lg mb-4'
+            alt={`${title} Image`}
           />
-          <p className='font-medium text-xl text-center'>{title}</p>
+          <p className='font-medium text-lg text-center'>{title}</p>
         </div>
       ))}
     </div>
-  )
+  );
 }
