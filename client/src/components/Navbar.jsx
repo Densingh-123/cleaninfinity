3f19b554e80493f8d54dcc531718a20546789194
shanker2004 <shdeveloper2004@gmailcom>
2024-10-23 23:45:45 +0530

    resp nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function Navbar() {
-  const location = useLocation(); // Get the current location
+  const location = useLocation()
+  const [isOpen, setIsOpen] = useState(false)
   const links = [
     { href: '/dashboard', label: 'Dashboard' },
     { href: '/awareness', label: 'Awareness' },
@@ -9,22 +11,39 @@ export default function Navbar() {
     { href: '/nfc', label: 'NFC Pairing' },
     { href: '/profile', label: 'Profile' },
     { href: '/', label: 'Log out' },
-  ];
+  ]
 
   return (
-    <header className="fixed top-0 w-full bg-black z-10 shadow-lg">
-      <div className="container mx-auto px-4 py-2 flex items-center justify-between">
-        <Link to="/dashboard">
-          <img src="/1.png" className="w-12 h-10" alt="Logo" />
+    <header className='fixed top-0 w-full bg-black z-10 shadow-lg'>
+      <div className='container mx-auto px-4 py-2 flex items-center justify-between'>
+        <Link to='/dashboard'>
+          <img
+            src='/1.png'
+            className='w-12 h-10'
+            alt='Logo'
+          />
         </Link>
-        <nav className="flex gap-4">
+
+        <button
+          className='lg:hidden text-dark-green focus:outline-none'
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? 'Close' : 'Menu'}
+        </button>
+
+        <nav
+          className={`flex-col lg:flex lg:flex-row ${
+            isOpen ? 'flex' : 'hidden'
+          }`}
+        >
           {links.map(({ href, label }) => (
             <Link
               key={href}
               to={href}
-              className={`text-green-500 hover:text-green-600 transition-colors duration-300 ${
+              className={`text-dark-green hover:text-green-600 transition-colors duration-300 px-4 py-2 ${
                 location.pathname === href ? 'font-semibold' : 'font-medium'
               }`}
+              onClick={() => setIsOpen(false)}
             >
               {label}
             </Link>
@@ -32,5 +51,5 @@ export default function Navbar() {
         </nav>
       </div>
     </header>
-  );
+  )
 }
