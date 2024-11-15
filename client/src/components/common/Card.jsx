28d1a70cc15ea41e77beccd19aca5b4ee28ff0fc
shanker2004 <shdeveloper2004@gmail.com>
2024-11-16 01:00:46 +0530

    card ui updation

--- client/src/components/common/Card.jsx
+++ client/src/components/common/Card.jsx
@@ -6,7 +6,7 @@ export default function Card({title, description, image, buttonEnable = true}) {
         alt='Card Image'
         className='w-full aspect-video object-cover'
       />
-      <div className='p-2 drop'>
+      <div className={`${buttonEnable ? "p-4" : "p-2"} drop`}>
         <h3 className='text-2xl font-semibold mb-2'>{title}</h3>
         <p className={buttonEnable ? "mb-4" : "mb-2"}>{description}</p>
         {buttonEnable && <button className='btn px-4 py-2'>Learn More</button>}
