03d58d8ad35227dc132e338ddc5083a6a7a78267
Chokkalingam.s <chok22017.it@rmkec.ac.in>
2024-12-05 22:48:06 +0530

    Railway mysql Try

--- server/models/db.js
+++ server/models/db.js
@@ -1,19 +1,11 @@
 const { Sequelize } = require('sequelize');
-const dbConfig = require('../config/db.config');
 
-const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  {
-    host: dbConfig.host,
-    dialect: dbConfig.dialect,
-    port: dbConfig.port,
-    dialectOptions: {
-      connectTimeout: 70000, 
-    },
-  }
-);
+const sequelize = new Sequelize('mysql://root:MkNrDoEhNglXspoUVaCTvJkTyrtqarKP@junction.proxy.rlwy.net:26168/railway', {
+  dialect: 'mysql',
+  dialectOptions: {
+    connectTimeout: 70000, 
+  },
+});
 
 sequelize.authenticate()
   .then(() => console.log('Database connected successfully.'))
